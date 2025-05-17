2598627a715c7fb715b36d0d585e5b7f69eb4a9b
Vaibhav Sharma <vaib.sharma44@gmail.com>
2025-05-18 02:37:39 +0530

    added crash simulation

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ import (
 	"github.com/vaibsharma/ddia/handler"
 )
 
+var (
+	stop chan os.Signal
+)
+
 func gracefulShutdown(server *http.Server, timeout time.Duration) {
 	// This function handles graceful shutdown of the HTTP server.
 	// It takes a pointer to an http.Server and a timeout duration as parameters.
@@ -24,16 +28,40 @@ func gracefulShutdown(server *http.Server, timeout time.Duration) {
 	}
 }
 
+func SimulateCrash(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Simulating crash..."))
+	stop <- os.Interrupt
+}
+
+func setupRoutes() *http.ServeMux {
+	// This function sets up the HTTP routes for the server.
+	// It returns a pointer to an http.ServeMux, which is a request multiplexer.
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		// This is a health check endpoint.
+		// It responds with a 200 OK status to indicate that the server is healthy.
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/", handler.HelloWorldHandler)
+	mux.HandleFunc("/crash", SimulateCrash)
+	return mux
+}
+
 func startServer() {
 	// This function starts the HTTP server.
 	// It creates a new http.Server instance and sets up the routes.
 	// The server listens on port 8080 and handles incoming requests.
+
+	// Server routes
+	mux := setupRoutes()
+
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: http.DefaultServeMux,
+		Handler: mux,
 	}
-
-	http.HandleFunc("/", handler.HelloWorldHandler)
 
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -43,10 +71,10 @@ func startServer() {
 
 	fmt.Println("Server started on :8080")
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	stop = make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
-	sig := <-c
+	sig := <-stop
 	fmt.Printf("Received signal: %s\n", sig)
 
 	gracefulShutdown(server, 5*time.Second)
@@ -59,6 +87,14 @@ func main() {
 	// For example, you can call other functions or perform operations.
 	// This is a placeholder for your main logic.
 
+	// panic recovery
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Recovered from panic: %v\n", r)
+		}
+	}()
+
 	// Create a new HTTP server and start it.
 	startServer()
+
 }
